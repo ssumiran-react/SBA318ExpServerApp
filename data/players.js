12a14938324ef259216a18103b94526f10a55bee
ssumiran <ssumiran@yahoo.com>
2026-06-18 20:56:57 -0400

    player data

---
+++ data/players.js
@@ -0,0 +1,213 @@
+const players = [
+    {
+        "id": 1,
+        "firstName":"OG",
+        "lastName": "Anunoby",
+        "teamId" : "NYK",
+        "number" : 8,
+        "position": [F,G],
+        "height" : 6.7,
+        "weight" : 240
+    },
+    {
+        "id": 2,
+        "firstName":"Mikal",
+        "lastName": "Bridges",
+        "teamId" : "NYK",
+        "number" : 25,
+        "position": [F,G],
+        "height" : 6.6,
+        "weight" : 209
+    },
+    {
+        "id": 3,
+        "firstName":"Jalen",
+        "lastName": "Brunson",
+        "teamId" : "NYK",
+        "number" : 11,
+        "position": [G],
+        "height" : 6.2,
+        "weight" : 190
+    },
+    {
+        "id": 4,
+        "firstName":"Jordan",
+        "lastName": "Clarkson",
+        "teamId" : "NYK",
+        "number" : 0,
+        "position": [G],
+        "height" : 6.5,
+        "weight" : 194
+    },
+    {
+        "id": 5,
+        "firstName":"Josh",
+        "lastName": "Hart",
+        "teamId" : "NYK",
+        "number" : 3,
+        "position": [G],
+        "height" : 6.5,
+        "weight" : 215
+    },
+    {
+        "id": 6,
+        "firstName":"Karl-Anthony",
+        "lastName": "Towns",
+        "teamId" : "NYK",
+        "number" : 32,
+        "position": [F,C],
+        "height" : 7.0,
+        "weight" : 248
+    },
+    {
+        "id": 7,
+        "firstName":"Jayson",
+        "lastName": "Tatum",
+        "teamId" : "BOS",
+        "number" : 0,
+        "position": [F,G],
+        "height" : 6.8,
+        "weight" : 210
+    },
+    {
+        "id": 8,
+        "firstName":"Jaylen",
+        "lastName": "Brown",
+        "teamId" : "BOS",
+        "number" : 7,
+        "position": [F,G],
+        "height" : 6.6,
+        "weight" : 223
+    },
+    {
+        "id": 9,
+        "firstName":"John",
+        "lastName": "Tonje",
+        "teamId" : "BOS",
+        "number" : 8,
+        "position": [G],
+        "height" : 6.4,
+        "weight" : 218
+    },
+    {
+        "id": 10,
+        "firstName":"Derrick",
+        "lastName": "White",
+        "teamId" : "BOS",
+        "number" : 9,
+        "position": [G],
+        "height" : 6.4,
+        "weight" : 190
+    },
+    {
+        "id": 11,
+        "firstName":"Payton",
+        "lastName": "Pritchard",
+        "teamId" : "BOS",
+        "number" : 11,
+        "position": [G],
+        "height" : 6.1,
+        "weight" : 195
+    },    
+    {
+        "id": 12,
+        "firstName":"Stephon",
+        "lastName": "Castle",
+        "teamId" : "SAS",
+        "number" : 5,
+        "position": [G],
+        "height" : 6.6,
+        "weight" : 215
+    },
+    {
+        "id": 13,
+        "firstName":"De'Aaron",
+        "lastName": "Fox",
+        "teamId" : "SAS",
+        "number" : 4,
+        "position": [G],
+        "height" : 6.3,
+        "weight" : 185
+    },
+    {
+        "id": 14,
+        "firstName":"Victor",
+        "lastName": "Wembanyama",
+        "teamId" : "SAS",
+        "number" : 1,
+        "position": [F,C],
+        "height" : 7.4,
+        "weight" : 235
+    },
+    {
+        "id": 15,
+        "firstName":"Trey",
+        "lastName": "Alexander",
+        "teamId" : "NOP",
+        "number" : 23,
+        "position": [G],
+        "height" : 6.5,
+        "weight" : 185
+    },
+    {
+        "id": 16,
+        "firstName":"Saddiq",
+        "lastName": "Bey",
+        "teamId" : "NOP",
+        "number" : 41,
+        "position": [G,F],
+        "height" : 6.8,
+        "weight" : 215
+    },
+    {
+        "id": 17,
+        "firstName":"Hunter",
+        "lastName": "Dickinson",
+        "teamId" : "NOP",
+        "number" : 4,
+        "position": [C],
+        "height" : 7.1,
+        "weight" : 255
+    },
+    {
+        "id": 18,
+        "firstName":"Jeremiah",
+        "lastName": "Fears",
+        "teamId" : "NOP",
+        "number" : 0,
+        "position": [G],
+        "height" : 6.3,
+        "weight" : 190
+    },
+    {
+        "id": 19,
+        "firstName":"Jordan",
+        "lastName": "Hawkins",
+        "teamId" : "NOP",
+        "number" : 24,
+        "position": [G],
+        "height" : 6.5,
+        "weight" : 190
+    },
+    {
+        "id": 20,
+        "firstName":"Herbert",
+        "lastName": "Jones",
+        "teamId" : "NOP",
+        "number" : 2,
+        "position": [F],
+        "height" : 6.7,
+        "weight" : 206
+    },
+    {
+        "id": 21,
+        "firstName":"DeAndre",
+        "lastName": "Jordan",
+        "teamId" : "NOP",
+        "number" : 9,
+        "position": [C],
+        "height" : 6.11,
+        "weight" : 265
+    },
+
+]
