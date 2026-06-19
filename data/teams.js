e8cc1483174be9c658b00c602b5aca43ab8f398a
ssumiran <ssumiran@yahoo.com>
2026-06-18 20:12:03 -0400

    teams and standings data

---
+++ data/teams.js
@@ -0,0 +1,193 @@
+const teams=[
+    {
+        "id" : "BOS",
+        "name" : "Boston Celtics",
+        "conference" : "Eastern",
+        "division" : "Atlantic" 
+    },
+    {
+        "id" : "BKN",
+        "name" : "Brooklyn Nets",
+        "conference" : "Eastern",
+        "division" : "Atlantic"
+    },
+    {
+        "id" : "NYK",
+        "name" : "New York Knicks",
+        "conference" : "Eastern",
+        "division" : "Atlantic"
+    },
+    {
+        "id" : "PHI",
+        "name" : "Philadelphia 76ers",
+        "conference" : "Eastern",
+        "division" : "Atlantic"
+    },
+    {
+        "id" : "TOR",
+        "name" : "Toronto Raptors",
+        "conference" : "Eastern",
+        "division" : "Atlantic"
+    },    
+    {
+        "id" : "CHI",
+        "name" : "Chicago Bulls",
+        "conference" : "Eastern",
+        "division" : "Central"
+    },
+    {
+        "id" : "CLE",
+        "name" : "Cleveland Cavaliers",
+        "conference" : "Eastern",
+        "division" : "Central"
+    },
+    {
+        "id" : "DET",
+        "name" : "Detroit Pistons",
+        "conference" : "Eastern",
+        "division" : "Central"
+    },
+    {
+        "id" : "IND",
+        "name" : "Indiana Pacers",
+        "conference" : "Eastern",
+        "division" : "Central"
+    },
+    {
+        "id" : "MIL",
+        "name" : "Milwaukee Bucks",
+        "conference" : "Eastern",
+        "division" : "Central"
+    },
+    {
+        "id" : "ATL",
+        "name" : "Atlanta Hawks",
+        "conference" : "Eastern",
+        "division" : "Southeast"
+    },
+    {
+        "id" : "CHA",
+        "name" : "Charlotte Hornets",
+        "conference" : "Eastern",
+        "division" : "Southeast"
+    },
+    {
+        "id" : "MIA",
+        "name" : "Miami Heat",
+        "conference" : "Eastern",
+        "division" : "Southeast"
+    },
+    {
+        "id" : "ORL",
+        "name" : "Orlando Magic",
+        "conference" : "Eastern",
+        "division" : "Southeast"
+    },
+    {
+        "id" : "WAS",
+        "name" : "Washington Wizards",
+        "conference" : "Eastern",
+        "division" : "Southeast"
+    },
+    {
+        "id" : "DEN",
+        "name" : "Denver Nuggets",
+        "conference" : "Western",
+        "division" : "Northwest"
+    },
+    
+    {
+        "id" : "MIN",
+        "name" : "Minnesota Timberwolves",
+        "conference" : "Western",
+        "division" : "Northwest"
+    },
+    {
+        "id" : "OKC",
+        "name" : "Oklahoma City Thunder",
+        "conference" : "Western",
+        "division" : "Northwest"
+    },
+    {
+        "id" : "POR",
+        "name" : "Portland Trail Blazers",
+        "conference" : "Western",
+        "division" : "Northwest"
+    },
+    {
+        "id" : "UTA",
+        "name" : "Utah Jazz",
+        "conference" : "Western",
+        "division" : "Northwest"
+    },
+    {
+        "id" : "GSW",
+        "name" : "Golden State Warriors",
+        "conference" : "Western",
+        "division" : "Pacific"
+    },
+    {
+        "id" : "LAC",
+        "name" : "Los Angeles Clippers",
+        "conference" : "Western",
+        "division" : "Pacific"
+    },
+    {
+        "id" : "LAL",
+        "name" : "Los Angeles Lakers",
+        "conference" : "Western",
+        "division" : "Pacific"
+    },
+    {
+        "id" : "PHO",
+        "name" : "Phoenix Suns",
+        "conference" : "Western",
+        "division" : "Pacific"
+    },
+    {
+        "id" : "SAC",
+        "name" : "Sacramento Kings",
+        "conference" : "Western",
+        "division" : "Pacific"
+    },
+    {
+        "id" : "DAL",
+        "name" : "Dallas Mavericks",
+        "conference" : "Western",
+        "division" : "Southwest"
+    },
+    {
+        "id" : "HOU",
+        "name" : "Houston Rockets",
+        "conference" : "Western",
+        "division" : "Southwest"
+    },
+    {
+        "id" : "MEM",
+        "name" : "Memphis Grizzlies",
+        "conference" : "Western",
+        "division" : "Southwest"
+    },
+    {
+        "id" : "NOP",
+        "name" : "New Orleans Pelicans",
+        "conference" : "Western",
+        "division" : "Southwest"
+    },
+    {
+        "id" : "SAS",
+        "name" : "San Antonio Spurs",
+        "conference" : "Western",
+        "division" : "Southwest"
+    }
+
+]
+
+
+
+
+
+
+
+
+
